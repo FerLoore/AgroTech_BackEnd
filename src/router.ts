60c013619be4396a228f4085f88a75ede95e792e
Fernando Lopez Orellana <digoferloorellana@gmail.com>
2026-03-09 20:51:44 -0600

    Se crea CRUD completo de la tabla Catalogo patogenos

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import agroRolRoutes from "./routes/agroRol.routes";
 import agroTipoArbolRoutes from "./routes/agroTipoArbol.routes";
+import agroCatalogoPatogenoRoutes from "./routes/agroCatalogoPatogeno.routes";
 
 // Cada dev agrega su línea aquí al terminar su módulo
 // Dev 1 → agroRol, agroTipoArbol, agroCatalogoPatogeno, agroProducto
@@ -12,5 +13,7 @@ const router = Router();
 
 router.use("/agro-roles", agroRolRoutes);
 router.use("/agro-tipo-arbol", agroTipoArbolRoutes);
+router.use("/agro-catalogo-patogeno",  agroCatalogoPatogenoRoutes);
+
 
 export default router;
