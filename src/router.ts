76afbe81d74a16075cebc3831f6a63b579c1b4a3
Fernando Lopez Orellana <digoferloorellana@gmail.com>
2026-03-09 20:15:41 -0600

    Se crea CRUD completo de la tabla Tipo Arboles

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import agroRolRoutes from "./routes/agroRol.routes";
+import agroTipoArbolRoutes from "./routes/agroTipoArbol.routes";
 
 // Cada dev agrega su línea aquí al terminar su módulo
 // Dev 1 → agroRol, agroTipoArbol, agroCatalogoPatogeno, agroProducto
@@ -10,5 +11,6 @@ import agroRolRoutes from "./routes/agroRol.routes";
 const router = Router();
 
 router.use("/agro-roles", agroRolRoutes);
+router.use("/agro-tipo-arbol", agroTipoArbolRoutes);
 
 export default router;
